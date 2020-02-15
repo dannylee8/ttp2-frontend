74753e0c65214281e9a42010fa6a1a01f1a6fcc0
Danny Lee <dreamer@panix.com>
2020-02-14 20:02:55 -0500

    Add menu buttons for portfolio and transactions

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,25 +14,31 @@ const Home = (props) => {
 
   return (
     <div>
-      <div className='top-menu'>
+      <div className='w3-bar top-menu'>
         <span className='logo'>TTP Stock Trade</span>
-        { props.loggedInStatus ? <Link to='/logout' onClick={handleClick}><button type="button" className="w3-btn w3-round-large w3-blue-grey">
-          <span className="glyphicon glyphicon-log-out"></span> Log Out </button></Link> : null }
+        {props.loggedInStatus
+          ?    
+          <>       
+            <Link to='/logout' onClick={handleClick}>
+              <button className="w3-bar-item w3-button w3-pale-blue w3-right w3-hover-pale-red"><span className='glyphicon glyphicon-log-out' /> Log Out</button>
+            </Link>
+            <button className="w3-bar-item w3-button w3-light-grey w3-right w3-hover-pale-red">Portfolio</button>
+            <button className="w3-bar-item w3-button w3-sand w3-right w3-hover-pale-red">Transactions</button>
+          </>
+          : null}
       </div>
-      {props.loggedInStatus ? 
-        <div className='row'>
+      {props.loggedInStatus
+        ? <div className='row'>
           <div className='column left'>
               Left
           </div>
           <div className='column right'>
               Right
           </div>
-        </div>
-        :
-        null
-      }
+          </div>
+        : null}
     </div>
   )
-};
+}
 
 export default Home
