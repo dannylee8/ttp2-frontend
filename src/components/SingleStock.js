ea30bd836d6d0de3720d7fdc123df2d38fcbd374
Danny Lee <dreamer@panix.com>
2020-02-17 00:41:38 -0500

    Add CSS color rendering depending on performance

--- src/components/SingleStock.js
+++ src/components/SingleStock.js
@@ -2,16 +2,21 @@ import React from 'react'
 var commaNumber = require('comma-number')
 
 function SingleStock (props) {
+  // conditionally render color of the stock Symbol and Price 
+  // red if current price is less than open price
+  // green if reverse, gray if equal
+  console.log(props)
+
   return (
     <tr>
-      <td>
+      <td className={(props.currentPrice > props.openPrice) ? 'green' : (props.currentPrice === props.openPrice) ? 'gray' : 'red'}>
         {props.symbol}
       </td>
       <td>
         {props.shares} shares
       </td>
-      <td>
-        $ {commaNumber((props.shares * props.price).toFixed(2))}
+      <td className={(props.currentPrice > props.openPrice) ? 'green' : (props.currentPrice === props.openPrice) ? 'gray' : 'red'}>
+        $ {commaNumber((props.shares * props.currentPrice).toFixed(2))}
       </td>
     </tr>
   )
